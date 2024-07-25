f96014a92165516e6fbaabf79b142ff88bb11af8
Kumar Sanyam <142786272+kumarsanyam01@users.noreply.github.com>
2024-07-26 03:57:59 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -12,7 +12,6 @@ export default function Home() {
   const [dropdown, setDropdown] = useState([])
 
   useEffect(() => {
-    // Fetch products on load 
     const fetchProducts = async () => {
       const response = await fetch('/api/product')
       let rjson = await response.json()
@@ -22,7 +21,6 @@ export default function Home() {
   }, [])
 
   const buttonAction = async (action, slug, initialQuantity) => {
-    // Immediately change the quantity of the product with given slug in Products
     let index = products.findIndex((item) => item.slug == slug)
     let newProducts = JSON.parse(JSON.stringify(products))
     if (action == "plus") {
@@ -32,7 +30,6 @@ export default function Home() {
     }
     setProducts(newProducts)
 
-    // Immediately change the quantity of the product with given slug in Dropdown
     let indexdrop = dropdown.findIndex((item) => item.slug == slug)
     let newDropdown = JSON.parse(JSON.stringify(dropdown))
     if (action == "plus") {
@@ -62,13 +59,13 @@ export default function Home() {
         body: JSON.stringify({ slug })
       });
 
-      // Remove the product from the state after deletion
       setProducts(newProducts.filter(product => product.slug !== slug));
       setDropdown(newDropdown.filter(product => product.slug !== slug));
     }
   }
 
   const addProduct = async (e) => {
+    e.preventDefault();
     try {
       const response = await fetch('/api/product', {
         method: 'POST',
@@ -79,39 +76,32 @@ export default function Home() {
       });
 
       if (response.ok) {
-        // Product added successfully
-        setAlert("Your Product has been added!")
-        setProductForm({})
+        setAlert("Your Product has been added!");
+        setProductForm({});
       } else {
-        // Handle error case
         console.error('Error adding product');
       }
     } catch (error) {
       console.error('Error:', error);
     }
-    // Fetch all the products again to sync back
-    const response = await fetch('/api/product')
-    let rjson = await response.json()
-    setProducts(rjson.products)
-    e.preventDefault();
+    
+    const response = await fetch('/api/product');
+    let rjson = await response.json();
+    setProducts(rjson.products);
   }
 
   const handleChange = (e) => {
-    setProductForm({ ...productForm, [e.target.name]: e.target.value })
+    setProductForm({ ...productForm, [e.target.name]: e.target.value });
   }
 
   const onDropdownEdit = async (e) => {
     let value = e.target.value;
-    console.log('Input value:', value); // Log the input value
-  
     setQuery(value);
     if (value.length > 0) {
       setLoading(true);
       setDropdown([]);
-      console.log('Searching for:', value); // Log the search query
       const response = await fetch('/api/search?query=' + value);
       let rjson = await response.json();
-      console.log('Search results:', rjson); // Log the search results
       setDropdown(rjson.products.filter(product => product.quantity > 0));
       setLoading(false);
     } else {
@@ -131,18 +121,16 @@ export default function Home() {
         </div>
         {loading && <div className='flex justify-center items-center'> <img width={74} src="/loading.svg" alt="" /> </div>}
         <div className="dropcontainer absolute w-[72vw] border-1 bg-indigo-100 rounded-md ">
-          {dropdown.map(item => {
-            return (
-              <div key={item.slug} className="container flex justify-between p-2 my-1 border-b-2 rounded-md">
-                <span className="slug"> {item.slug} ({item.quantity} available ₹{item.price} each)</span>
-                <div className='mx-5'>
-                  <button onClick={() => { buttonAction("minus", item.slug, item.quantity) }} disabled={loadingaction} className="subtract inline-block px-3 py-1 cursor-pointer bg-indigo-500 text-white font-semibold rounded-lg shadow-md disabled:bg-indigo-200"> - </button>
-                  <span className="quantity inline-block min-w-3 mx-3">{item.quantity}</span>
-                  <button onClick={() => { buttonAction("plus", item.slug, item.quantity) }} disabled={loadingaction} className="add inline-block px-3 py-1 cursor-pointer bg-indigo-500 text-white font-semibold rounded-lg shadow-md disabled:bg-indigo-200">  + </button>
-                </div>
+          {dropdown.map(item => (
+            <div key={item.slug} className="container flex justify-between p-2 my-1 border-b-2 rounded-md">
+              <span className="slug"> {item.slug} ({item.quantity} available ₹{item.price} each)</span>
+              <div className='mx-5'>
+                <button onClick={() => { buttonAction("minus", item.slug, item.quantity) }} disabled={loadingaction} className="subtract inline-block px-3 py-1 cursor-pointer bg-indigo-500 text-white font-semibold rounded-lg shadow-md disabled:bg-indigo-200"> - </button>
+                <span className="quantity inline-block min-w-3 mx-3">{item.quantity}</span>
+                <button onClick={() => { buttonAction("plus", item.slug, item.quantity) }} disabled={loadingaction} className="add inline-block px-3 py-1 cursor-pointer bg-indigo-500 text-white font-semibold rounded-lg shadow-md disabled:bg-indigo-200">  + </button>
               </div>
-            );
-          })}
+            </div>
+          ))}
         </div>
       </div>
 
@@ -151,7 +139,7 @@ export default function Home() {
 
         <form>
           <div className="mb-4">
-            <label htmlFor="productName" className="block mb-2">Product Slug</label>
+            <label htmlFor="productName" className="block mb-2">Product</label>
             <input value={productForm?.slug || ""} name='slug' onChange={handleChange} type="text" id="productName" className="w-full border border-gray-300 px-4 py-2 rounded-md" />
           </div>
 
@@ -165,6 +153,11 @@ export default function Home() {
             <input value={productForm?.price || ""} name='price' onChange={handleChange} type="number" id="price" className="w-full border border-gray-300 px-4 py-2 rounded-md" />
           </div>
 
+          <div className="mb-4">
+            <label htmlFor="email" className="block mb-2">Email</label>
+            <input value={productForm?.email || ""} name='email' onChange={handleChange} type="email" id="email" className="w-full border border-gray-300 px-4 py-2 rounded-md" />
+          </div>
+
           <button onClick={addProduct} type="submit" className="bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg shadow-md font-semibold">
             Add Product
           </button>
@@ -172,7 +165,7 @@ export default function Home() {
       </div>
       
       <div id="display" className="container my-24 mx-auto px-24">
-        <h1 className="heading text-3xl font-semibold mb-6 text-center">Display Current Stock</h1>
+        <h1 className="heading text-3xl font-semibold mb-6 text-center">Products Available for Purchase</h1>
 
         <table className="table-auto w-full">
           <thead>
@@ -180,25 +173,25 @@ export default function Home() {
               <th className="px-4 py-2">Product Name</th>
               <th className="px-4 py-2">Quantity</th>
               <th className="px-4 py-2">Price</th>
+              <th className="px-4 py-2">Email</th>
             </tr>
           </thead>
           <tbody>
-            {products.map(product => {
-              return (
-                <tr key={product.slug} className="rounded-md">
-                  <td className="border px-4 py-2 rounded-md">{product.slug}</td>
-                  <td className="border px-4 py-2 rounded-md">{product.quantity}</td>
-                  <td className="border px-4 py-2 rounded-md">₹{product.price}</td>
-                </tr>
-              )
-            })}
+            {products.map(product => (
+              <tr key={product.slug} className="rounded-md">
+                <td className="border px-4 py-2 rounded-md">{product.slug}</td>
+                <td className="border px-4 py-2 rounded-md">{product.quantity}</td>
+                <td className="border px-4 py-2 rounded-md">₹{product.price}</td>
+                <td className="border px-4 py-2 rounded-md">{product.email}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
 
       <style jsx>{`
         .heading {
-          font-size: 1.875rem; /* default to text-3xl */
+          font-size: 1.875rem;
         }
 
         @media (max-width: 575px) and (min-width: 280px) {
@@ -208,25 +201,10 @@ export default function Home() {
           .dropcontainer {
             width: 100%;
           }
-          input[type="text"], input[type="number"] {
-            width: 100%;
-            padding: 0.5rem;
-            margin-bottom: 1rem;
-          }
-          table {
-            width: 100%;
-          }
-          th, td {
-            padding: 0.5rem;
-            font-size: 0.8rem;
-          }
-          button {
+          input[type="text"], input[type="number"], input[type="email"] {
             width: 100%;
             padding: 0.5rem;
             font-size: 1rem;
-          }
-          .heading {
-            font-size: 1.5rem; /* smaller heading size for small screens */
           }
         }
       `}</style>
